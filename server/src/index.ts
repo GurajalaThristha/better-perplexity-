f052e6cffdededae4138ef15a2f6e7cd059af8d1
Thristha <gurajalathristha@example.com>
2026-03-17 01:16:16 -0400

    Enable CORS for deployed frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json());
 
 const openai = new OpenAI({
